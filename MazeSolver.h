b63bc0e1c239375d28fb5e41fdf6796041f28989
Muhammad Talha Dar <tdtalhadar@gmail.com>
2020-11-20 12:57:37 +0500

    implemented maze solution, driver code. no tests

--- MazeSolver.h
+++ MazeSolver.h
@@ -4,166 +4,277 @@
 #include <iostream>
 using namespace std;
 
-struct Node{
-	int x, y, val;
-	char direction; // U,R,D,L
 
-	Node(const int _x = 0, const int _y = 0, const int _val = 1){
-		x = _x, y = _y, val = _val, direction = 'R';
-	}
+/*
+  MISC FUNCTIONS
+*/
+template<typename T>
+int stackSize(Stack<T> stk){
+  int i=0;
+  while(!stk.empty()){
+    stk.pop();
+    i++;
+  }
+  return i;
+}
+
+
+/*
+  struct Node is a single location in our maze indexed as (x,y).
+*/
+struct Node {
+  int x, y;
+  int direction; // 0=up, 1=right, 2=down, 3=left
+
+  Node(const int _x = -1, const int _y = -1, const int _direction=-1) {
+    x = _x, y = _y,direction = 0;
+  }
+  
+  int getX()const{
+    return x;
+  }
+  int getY()const{
+    return 0;
+  }
+
 };
 
+ostream& operator<<(ostream& out, const Node& obj){
+ return  out<<'('<<obj.x<<','<<obj.y<<')';
+}
+
+
+/*
+  Maze as a class.
+*/
 class Maze {
 private:
-	int **maze;
-	int rows;
-	int columns;
-	bool solved;
+  // maze as a 2d array
+  int **maze;
+  // maze dimensions
+  int rows;
+  int columns;
+  // wether maze has been solved or not.
+  bool solved;
 
 public:
-	// default constructor
-	Maze();
-	Maze(const int *);
-	// destructor
-	~Maze();
+  // default constructor
+  Maze();
+  
+  // destructor
+  ~Maze();
 
-	// *****methods***** //
+  // *****methods***** //
 
-	// read and write
+  // read and write
 
-	// reads a file at argument path
-	bool readMaze(const char *);
+  // reads a file at argument path
+  bool readMaze(const char *);
 
-	// writes current maze to a file give at path(creates the file to write, if it
-	// doesnt already exist.)
-	bool writeMaze(const char *) const;
+  // writes current maze to a file give at path(creates the file to write, if it
+  // doesnt already exist.)
+  bool writeMaze(const char *) const;
 
-	// prints out the current maze to console
-	bool visualizeMaze() const;
+  // prints out the current maze to console
+  bool visualizeMaze() const;
 
-	// finds a path in maze from start to finish.
-	bool solveMaze();
-
+  // finds a path in maze from start to finish
+  // returns solution as an array of struct Node.
+  Node *solveMaze();
 };
 
 Maze::Maze() {
-	this->maze = nullptr;
-	this->rows = 0, this->columns = 0;
-	this->solved = false;
+  this->maze = nullptr;
+  this->rows = 0, this->columns = 0;
+  this->solved = false;
 }
 
 // destructor
 Maze::~Maze() {
-	if (this->maze) {
-		// delete memory at row
-		for (int i = 0; i < this->rows; i++){
-			delete[] maze[i];
-			maze[i] = nullptr;
-		}
-		delete[] maze;
-		maze = nullptr;
-	}
+  if (this->maze) {
+    // delete memory at each row
+    for (int i = 0; i < this->rows; i++) {
+      delete[] this->maze[i];
+      this->maze[i] = nullptr;
+    }
+    delete[] this->maze;
+    this->maze = nullptr;
+  }
 }
 
 // methods
 
 bool Maze::readMaze(const char *path) {
-	// const char* path: path of txt file where maze is to be read
+  // const char* path: path of txt file where maze is to be read
 
-	if (path) {
+  if (path) {
 
-		// open maze file
-		ifstream fin(path);
-		if (!fin.is_open()) {
-			return false;
-		}
+    // open maze file
+    ifstream fin(path);
+    if (!fin.is_open()) {
+      return false;
+    }
 
-		// get maze dimensions (x,y) , from first line of file.
-		fin >> rows;
-		fin >> columns;
+    // get maze dimensions (x,y) , from first line of file.
+    fin >> rows;
+    fin >> columns;
 
-		// create maze rows
-		maze = new int*[rows]{nullptr};
-		// create maze columns on each row
-		for (int i = 0; i < rows; i++){
-			maze[i] = new int[columns];
-		}
+    // create maze rows
+    maze = new int *[rows] { nullptr };
+    // create maze columns on each row
+    for (int i = 0; i < rows; i++) {
+      maze[i] = new int[columns];
+    }
 
-		// read from file to array
-		for (int i = 0; i < rows; i++){
-			for (int j = 0; j < columns; j++){
-				fin >> maze[i][j];
-			}
-		}
+    // read from file to array
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns; j++) {
+        fin >> maze[i][j];
+      }
+    }
 
-		// close maze file
-		fin.close();
-	}
+    // close maze file
+    fin.close();
+  }
 
-	//
-	return true;
+  //
+  return true;
 }
 
 bool Maze::visualizeMaze() const {
-	if (maze) {
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < columns; j++){
-				cout << maze[i][j] << "\t";
-			}
-			cout << endl;
-		}
-		return true;
-	}
-	return false;
+  if (maze) {
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns; j++) {
+        cout << maze[i][j] << "\t";
+      }
+      cout << endl;
+    }
+    return true;
+  }
+  return false;
 }
 
 bool Maze::writeMaze(const char *path) const {
-	//if (maze) {
-	//	//if (path) {
-	//	//  // create file to write at path, or open to append
-	//	//  fstream fout(path, ios::app | ios::out);
+  if (maze) {
+    if (path) {
+      // create file to write at path, or open to append
+      fstream fout(path, ios::app | ios::out);
 
-	//	//  // write maze dimensions to file
-	//	//  fout << x << " " << y << endl;
+      // write maze dimensions to file
+      fout << rows << " " << columns << endl;
 
-	//	//  // write maze to file
-	//	//  for (int i = 0; i < mazeSize; i++) {
-	//	//    fout << maze[i];
-	//	//    // if insertion is at end of row, insert new line, else insert
-	//	//    // whitespace
-	//	//    if ((i + 1) % y == 0) {
-	//	//      fout << endl;
-	//	//    } else {
-	//	//      fout << " ";
-	//	//    }
-	//	//  }
+      // write maze to file
+      for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+          fout << maze[i][j] << " ";
+        }
+        fout << endl;
+      }
 
-	//	//  fout.close();
-	//	//  return true;
-	//	//}
-	//}
-	return false;
+      fout.close();
+      return true;
+    }
+  }
+  return false;
 }
 
-bool Maze::solveMaze() {
+// returns a solution path as a char array
+Node* Maze::solveMaze() {
 
-	//Stack<Node> path;
-	//int destination = mazeSize - 1;
-	//int temp = 0;
-	//int x = 0, y = 0;
-	//Node currentNode(0, 0, 1); // location (0,0) and val=1
+  // the solution array to be returned
+  Node *solution = nullptr;
+  int solutionSize=0;
 
-	//path.push(currentNode);
+  // the stack for backtracking
+  Stack<Node> path;
 
-	//while (!path.empty()){
-	//	if (currentNode.direction == 'U'){
-	//		// check node above 
-	//		temp = currentNode.x + currentNode.y - this->y;
-	//		if (this->maze[temp] == 0){
-	//			currentNode.direction = 'R';
-	//		}
-	//	}
-	//	else if (currentNode.direction)
-	//}
-	return true;
+  if (maze && !solved) {
+    Node top(0, 0,0);
+
+    path.push(top);
+
+    // if we back tracked all the way back to start, it means there was no path
+    // forward, hence no solution.
+    while (!path.empty()) {
+
+      top = path.head();
+      // if our latest move is our destination node, break loop.
+      if (top.x == this->rows - 1 && top.y == this->columns - 1) {
+        this->solved = true;
+        break;
+      }
+      
+      // update our path's top's direction by replacing it with an update Node.
+      // using a nameless temporary object for that purpose.
+      path.pop();
+      path.push(Node(top.x, top.y, top.direction++));
+
+      // for each direction from our path's top,
+      // check if a move was valid, then make that move.
+
+      //upwards
+      if (top.direction == 0) {
+        if (top.x - 1 >= 0 && maze[top.x - 1][top.y] != 0) {
+          // mark current location as visited
+          maze[top.x][top.y] = -1;
+          // move up
+          top.x--;
+          // push new location to stack
+          path.push(top);
+        }
+      }
+      // rightwards
+      else if (top.direction == 1) {
+        if (top.y + 1 < this->columns && maze[top.x][top.y + 1] != 0) {
+	  // mark current location as visited
+	  maze[top.x][top.y] = -1;
+	  // move right
+          top.y++;
+          // push new location to stack
+          path.push(top);
+        }
+      } // downwards
+      else if (top.direction == 2) {
+	  if (top.x + 1 < this->rows && maze[top.x+1][top.y] != 0) {
+	  // mark current location as visited
+	  maze[top.x+1][top.y] = -1;
+	  // move down
+          top.x++;
+          // push new location to stack
+          path.push(top);
+        }
+      }
+      // leftwards
+      else if(top.direction==3){
+	 if (top.y-1 > 0 && maze[top.x][top.y-1] != 0) {
+	  // mark current location as visited
+	  maze[top.x][top.y-1] = -1;
+	  // move left
+          top.y--;
+          // push new location to stack
+          path.push(top);
+        }
+      }
+      // if no direction was moveable,
+      // mark current node as blocked, and backtrack.
+      else{
+	maze[top.x][top.y]=0;
+	// pop current location, top will now backtrack to previous visited node..
+	path.pop();
+      }
+    }
+
+    if (!path.empty()) {
+      solutionSize=stackSize<Node>(path);
+      solution=new Node[solutionSize+1];
+
+      // Node(-1,-1-,1) is taken as array delimiter
+      solution[--solutionSize]=Node();
+      //
+      for (int i=solutionSize-1; i>=0; i--){
+	solution[i]=path.pop();
+      }
+    }
+  }
+  return solution;
 }
--- main.cpp
+++ main.cpp
@@ -2,10 +2,19 @@
 #include"MazeSolver.h"
 using namespace std;
 
+
 int main(){
   Maze m1;
   m1.readMaze("maze.txt");
   m1.visualizeMaze();
-  cout << endl;
+
+  Node* sol=m1.solveMaze();
+
+  for(int i=0; sol[i].direction!=-1;i++){
+    cout<<sol[i];
+    if(sol[i+1].direction!=-1){
+      cout<<" -> ";
+    }
+  }
   return 0;
 }
