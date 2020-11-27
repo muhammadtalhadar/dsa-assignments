594131be47328e312711e7dd313f4f9e2acbe0be
Muhammad Talha Dar <tdtalhadar@gmail.com>
2020-11-27 14:26:58 +0500

    implemented search()

--- MyCircularLinkedList.h
+++ MyCircularLinkedList.h
@@ -3,7 +3,7 @@
 #include <iostream>
 using namespace std;
 
-template <class T> class MyCircularLinkedList :public CircularLinkedList<T> {
+template <class T> class MyCircularLinkedList : public CircularLinkedList<T> {
 
 public:
   // constructor
@@ -22,13 +22,13 @@ public:
   virtual bool deleteAValue(T) override;
 
   // searching
-  virtual bool search(T val) override{return true;}
-  virtual int searchPositionOfValue(T val) override{return -1;}
+  virtual bool search(T) override;
+  virtual int searchPositionOfValue(T) override{return 0;}
 
   // sorting
   virtual void sortCircularListInAscendingOrder() override{}
   virtual void sortCircularListInDescendingOrder() override{}
-  
+
   // misc
   virtual void print() override;
 };
@@ -44,136 +44,150 @@ MyCircularLinkedList<T>::MyCircularLinkedList() : CircularLinkedList<T>() {}
 //   MyCircularLinkedList<T>::tail=nullptr;
 // }
 
-
-
 /*
   Insertion
 */
 
-//inserts a node at start of LL
+// inserts a node at start of LL
 template <class T> void MyCircularLinkedList<T>::insertFirstNode(T data) {
 
   // if CLL is empty
   if (!this->tail) {
     this->tail = new Node<T>(data);
-    this->tail->next_=this->tail;
+    this->tail->next_ = this->tail;
   }
   // if CLL is not empty
   else {
     Node<T> *temp = this->tail->next_;
-    this->tail->next_=new Node<T>(data, temp);
+    this->tail->next_ = new Node<T>(data, temp);
   }
 }
 
 // inserts a new node as tail
-template<class T> void MyCircularLinkedList<T>::insertAtTail(T data){
+template <class T> void MyCircularLinkedList<T>::insertAtTail(T data) {
 
   // if CLL is empty
-  if(!this->tail){
-    this->tail=new Node<T>(data);
-    this->tail->next_=this->tail;
-  }
-  else{
+  if (!this->tail) {
+    this->tail = new Node<T>(data);
+    this->tail->next_ = this->tail;
+  } else {
     // backup next of tail
-    Node<T>* temp=this->tail->next_;
+    Node<T> *temp = this->tail->next_;
     // insert new node at next of tail with it's next set to next of tail
-    this->tail->next_=new Node<T>(data, temp);
+    this->tail->next_ = new Node<T>(data, temp);
     // move tail forward to point to new node
-    this->tail=this->tail->next_;
+    this->tail = this->tail->next_;
   }
 }
 
 /*
   Deletion
-*/ 
+*/
 
-template<class T>
-bool MyCircularLinkedList<T>::deleteFirstNode(){
+template <class T> bool MyCircularLinkedList<T>::deleteFirstNode() {
   // if list was empty
-  if(!this->tail){
+  if (!this->tail) {
     return false;
   }
 
   // if there was only one node
-  if(this->tail->next_==this->tail){
+  if (this->tail->next_ == this->tail) {
     delete[] this->tail;
-    this->tail=nullptr;
+    this->tail = nullptr;
     return true;
   }
-  
-  Node<T>* todelete=nullptr;
-  Node<T>* newFirst=nullptr;
 
-  todelete=this->tail->next_;
-  newFirst=todelete->next_;
+  Node<T> *todelete = nullptr;
+  Node<T> *newFirst = nullptr;
+
+  todelete = this->tail->next_;
+  newFirst = todelete->next_;
 
   delete todelete;
 
-  this->tail->next_=newFirst;
+  this->tail->next_ = newFirst;
 
   return true;
 }
 
-template<class T>
-bool MyCircularLinkedList<T>::deleteFromTail(){
+template <class T> bool MyCircularLinkedList<T>::deleteFromTail() {
   // if list is empty
-  if(!this->tail){
+  if (!this->tail) {
     return false;
   }
 
   // if had only one Node
-  if(this->tail==this->tail->next_){
+  if (this->tail == this->tail->next_) {
     delete this->tail;
-    this->tail=nullptr;
+    this->tail = nullptr;
     return true;
   }
 
-  Node<T>* head=this->tail->next_;//first element of list, pointes at by tail
-  Node<T>* next=this->tail;
+  Node<T> *head = this->tail->next_; // first element of list, pointes at by
+                                     // tail
+  Node<T> *next = this->tail;
 
   // point next to second last element of list
-  while(next->next_!=this->tail){
-    next=next->next_;
+  while (next->next_ != this->tail) {
+    next = next->next_;
   }
 
-  //delete tail
+  // delete tail
   delete this->tail;
 
   // point tail to second last(now last) node of list
-  this->tail=next;
-  
+  this->tail = next;
+
   // point new tail to head
-  this->tail->next_=head;
-  
+  this->tail->next_ = head;
+
   return true;
 }
 
-template<class T>
-bool MyCircularLinkedList<T>::deleteAValue(T data){
-  if(!this->tail){
+template <class T> bool MyCircularLinkedList<T>::deleteAValue(T data) {
+  if (!this->tail) {
     return false;
   }
 
-  //if our data was located at tail
-  if(this->tail->data_ == data){
+  // if our data was located at tail
+  if (this->tail->data_ == data) {
     return this->deleteFromTail();
   }
-  
+
   //
-  Node<T>* previous=this->tail; // the Node bhind the node to be deleted
-  Node<T>* toDelete=previous->next_; // the node to be deleted
+  Node<T> *previous = this->tail;      // the Node bhind the node to be deleted
+  Node<T> *toDelete = previous->next_; // the node to be deleted
 
-  while(toDelete!=this->tail){
+  while (toDelete != this->tail) {
 
-    if(toDelete->data_==data){
-      previous->next_=toDelete->next_;
+    if (toDelete->data_ == data) {
+      previous->next_ = toDelete->next_;
       delete toDelete;
       return true;
     }
-    
-    previous=previous->next_;
-    toDelete=previous->next_;
+
+    previous = previous->next_;
+    toDelete = previous->next_;
   }
+  return false;
+}
+
+// searching
+
+template <typename T> bool MyCircularLinkedList<T>::search(T val) {
+  if (!this->tail) {
+    return false;
+  }
+
+  Node<T> *temp = this->tail;
+
+  do {
+    if (temp->data_ == val) {
+      return true;
+    }
+    temp = temp->next_;
+  } while (temp != this->tail);
+
   return false;
 }
 
@@ -183,14 +197,13 @@ template <class T> void MyCircularLinkedList<T>::print() {
     cout << "NULL";
   } else {
     Node<T> *next = this->tail;
-    do{
-      next=next->next_;
-      cout<<next->data_;
+    do {
+      next = next->next_;
+      cout << next->data_;
 
-      if(next!=this->tail){
-        cout<<" -> ";
+      if (next != this->tail) {
+        cout << " -> ";
       }
-    }while (next!=this->tail);
-    
+    } while (next != this->tail);
   }
 }
--- main.cpp
+++ main.cpp
@@ -7,11 +7,16 @@ int main(){
   cout<<"\t========== Driver Code ========== "<<endl<<endl;
 
   MyCircularLinkedList<int> cll;
+
+  cout<<endl<<"\tINSERTIONS"<<endl;
+  
   cll.insertAtTail(10);
   cll.insertFirstNode(9);
   cll.insertAtTail(11);
   cll.insertFirstNode(8);
   cll.print();
+
+  cout<<endl<<endl<<"\tDELETIONS";
   cout<<endl<<"deletion of first: "<<cll.deleteFirstNode()<<endl;
   cll.print();
   cout<<endl<<"deletion at tail: "<<cll.deleteFromTail()<<endl;
@@ -29,5 +34,13 @@ int main(){
   cout<<endl<<"delete non-existing value: "<<cll.deleteAValue(360)<<endl;
   cll.print();
   cout<<endl<<"delete value 9(empty out the list, testing circular tail case): "<<cll.deleteAValue(9)<<endl;
+
+
+  cout<<endl<<endl<<"\tDELETIONS";
+  
+  cll.insertAtTail(100);
+  cout<<endl<<"After new insertions: "<<endl;
+  cll.print();
+  cout<<endl<<"search value(for circular tail); "<<cll.search(100)<<endl;
   return 0;
 }
